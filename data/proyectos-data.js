966f05dabbde1d9392f5fc60d7011904f7939046
castilloeme <emecasti65@gmail.com>
2024-11-24 16:34:47 -0500

    Agregando proyecto nitronix

--- data/proyectos-data.js
+++ data/proyectos-data.js
@@ -8,7 +8,23 @@ export const dataProyectos = [
     github: "https://github.com/emerson204/Uiwork",
     linkProyecto: "https://uiwork.vercel.app/",
     descripcion:
-      "🍞 Una hermosa biblioteca de componentes con Tailwind para React",
-    tecnologias: ["Next.js", "Tailwind", "Shadcn", "Framer Motion"],
+      "🍞 Una hermosa biblioteca de componentes con Tailwind para React.",
+    tecnologias: [
+      "React",
+      "Next.js",
+      "Tailwind CSS",
+      "Shadcn",
+      "Framer Motion",
+    ],
+  },
+  {
+    id: 2,
+    title: "Nitronix",
+    icon: Workflow,
+    github: "https://github.com/emerson204/nitronix",
+    linkProyecto: "https://nitronix.vercel.app/",
+    descripcion:
+      "🔧  Página moderna para taller mecánico, con diseño atractivo y profesional.",
+    tecnologias: ["React", "TypeScript", "Next.js", "Tailwind CSS", "Shadcn"],
   },
 ];
